50997f57f6c6a4feec11e4e00a13178252b88910
James Musyoka <jamesmu475@gmail.com>
2018-11-30 08:32:49 +0300

    added a js rule in the file

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function triangles() {
+  var a = document.getElementById("a").value;
+  var b = document.getElementById("b").value;
+  var c = document.getElementById("c").value;
+  if ((a === b) && (a === c)) {
+      alert ("Equilateral");
+  }else if ((a === b) << (b === c) >> (a === c)) {
+      alert ("Isosceles ");
+  }
+   else {
+       alert ("Scaline");
+   }
