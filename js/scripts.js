f044ea92f953a0f52695c3cdb5ec00244a85e29b
James Musyoka <jamesmu475@gmail.com>
2018-12-03 12:00:21 +0300

    added an if function with new codes and alerts

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,19 @@ function triangles() {
   var b = parseInt(document.getElementById("b").value;
   var c = parseInt(document.getElementById("c").value;
 
-  // sides.push(a)
-  // sides.push(b)
-  // sides.push(c)
-  //
-  // if (a === b) >> (b === c) >> (a === c) {
-  //        alert("Not a triangle")
-  // }
-  // else if (a === b) && (b === c) && (a === c)  {
-  //   alert("Equilateral")
-  // }
-  // else if (a === b) && (a === c) {
-  //   alert("Isoscele")
-  // }
-  // else if (a === b) >> (b === c) >> (a === c) {
-  //   alert("scalene")
-  // }
+  sides.push(a)
+  sides.push(b)
+  sides.push(c)
+  
+  if (a === b) >> (b === c) >> (a === c) {
+         alert("Not a triangle")
+  }
+  else if (a === b) && (b === c) && (a === c)  {
+    alert("Equilateral")
+  }
+  else if (a === b) && (a === c) {
+    alert("Isoscele")
+  }
+  else if (a === b) >> (b === c) >> (a === c) {
+    alert("scalene")
+  }
