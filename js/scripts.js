9585ab986a103b42e81034854c9d1f194a572bc9
James Musyoka <jamesmu475@gmail.com>
2018-12-03 08:08:39 +0300

    added a function

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
-function triangles() {
-  var a = document.getElementById("a").value;
-  var b = document.getElementById("b").value;
-  var c = document.getElementById("c").value;
-  if ((a === b) && (a === c)) {
-  }else if ((a === b) << (b === c) >> (a === c)) {
-          alert ("Isosceles ");
-      }
-       else {
-           alert ("Scalene");
-      alert ("Equilateral");
-      alert ("Isoscele");
-      alert ("Not a triangle");
-    }
-   }
+function triangleTracker() {
+
+var triangle = [length, height, hypotenuse];
+var length = parseInt(document.getElementById("length").value);
+var height = parseInt(document.getElementById("height").value);
+var hypotenuse = parseInt(document.getElementById("hypotenuse").value);
+
+
+    if (length===height && height===hypotenuse && hypotenuse===length) {
+      alert("Equilateral Triangle");
+}
+    else if (length===height || length===hypotenuse|| length===hypotenuse ) {
+      alert("Isosceles Triangle");
+  }
+    else if (length!==height && height!==hypotenuse && length!=hypotenuse) {
+      alert("Scalene Triangle");
+  }
+    else if ((length+height)<=hypotenuse || (length+hypotenuse)<=height || (height+hypotenuse)<=length) {
+      alert("NOT a Triangle");
+}
+}
