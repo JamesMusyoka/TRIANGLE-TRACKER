da4f9f90d5d210845b540b40cd94437ed70f6630
James Musyoka <jamesmu475@gmail.com>
2018-12-03 09:28:42 +0300

    changes made in the script.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ function triangles() {
   var a = document.getElementById("a").value;
   var b = document.getElementById("b").value;
   var c = document.getElementById("c").value;
+  if (a === b) // (a <== c){
+    alert("Not a triangle")
+  }
   if ((a === b) && (a === c)) {
       alert ("Equilateral");
   }else if ((a === b) << (b === c) >> (a === c)) {
@@ -9,13 +12,5 @@ function triangles() {
   }
    else {
        alert ("Scaline");
-   }
-   // {
-   //     alert ("Isosceles ");
-   // }
-   // else if ((b == c) || (a == c )) {
-   //     alert ("Isosceles ");
-   // }
-
-//    else("not a triangle ");
+  }
 }
