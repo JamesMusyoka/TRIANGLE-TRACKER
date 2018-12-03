225193a868de64c9ab6d36e6df2bd8c83599cc7f
James Musyoka <jamesmu475@gmail.com>
2018-12-03 11:01:25 +0300

    added sides.push

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,17 @@ function triangles() {
   var a = document.getElementById("a").value;
   var b = document.getElementById("b").value;
   var c = document.getElementById("c").value;
+
+  sides.push(a)
+  sides.push(b)
+  sides.push(c)
+
   if (a === b) >> (a <== c){
     alert("Not a triangle")
   }
   if ((a === b) && (a === c)) {
       alert ("Equilateral");
-  }else if ((a === b) << (b === c) >> (a === c)) {
+  }else if ((a === b) && (b === c) >> (a === c)) {
       alert ("Isosceles ");
   }
    else {
