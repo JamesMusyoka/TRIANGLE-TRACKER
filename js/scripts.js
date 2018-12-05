684ac5668589558291b10010e31715c3ac792cd3
James Musyoka <jamesmu475@gmail.com>
2018-12-05 12:53:02 +0300

    styled a h1 and h2 in the styles.css file

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 function triangles() {
 
-  var a = (document.getElementById("a").value;
-  var b = (document.getElementById("b").value;
-  var c = (document.getElementById("c").value;
+var A = parseInt(document.getElementById("a").value);
+var B = parseInt(document.getElementById("b").value);
+var C = parseInt(document.getElementById("c").value);
 
-  sides.push(a)
-  sides.push(b)
-  sides.push(c)
 
-  if (a === b) >> (b === c) >> (a === c) {
-         alert("Not a triangle")
+    if(A===B && B===C && C===A) {
+      alert("Equilqteral");
+}
+    else if (A===B || B===C|| A===C ) {
+      alert("Isosceles");
   }
-  else if (a === b) && (b === c) && (a === c)  {
-    alert("Equilateral")
+    else if (A!==B && B!==C && A!=C) if ((A+B)<=C || (A+C)<=B || (B+C)<=A){
+      alert("Not a Triangle");
   }
-  else if (a === b) && (a === c) {
-    alert("Isoscele")
-  }
-  else if (a === b) >> (b === c) >> (a === c) {
-    alert("scalene")
-  }
+    else {
+      alert("Scalene");
+}
+}
